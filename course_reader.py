1e9eaf5187f0201b61a0b2d62415f4150ebb28e8
Jiayi Wang <jw6dz@virginia.edu>
2015-02-23 15:08:16 -0500

    rough draft of load db method created

--- course_reader.py
+++ course_reader.py
@@ -1,9 +1,21 @@
+import sqlite3 as lite
 import csv
+import sys
 
 def load_course_database(db_name, csv_filename):
     f = open(csv_filename)
     csv_f = csv.reader(f)
-    for row in csv_f: #every row is a list
-        print(row)
+    con = lite.connect(db_name)
+    with con:
+         cur = con.cursor()
+         cur.execute("CREATE TABLE coursedata(deptID TEXT, courseNum INT, semester INT, meetingType TEXT, seatsTaken INT, seatsOffered INT, instructor TEXT)")
+    #for row in csv_f: #every row is a list
+    #    print(row)
+    for row in csv_f:
+        IDtup = (row[0], row[1])
+        with con:
+            cur = con.cursor()
+            sql_cmd = "INSERT INTO coursedata values(?. ?. row[2], row[3], row[4]. row[5]. row[6])"
+            cur.execute(sql_cmd, IDtup)
 
 load_course_database("hi", "seas-courses-5years.csv")
