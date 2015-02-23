c21d1e8597ce42140c650e6b7a9a1887545ad532
Jiayi Wang <jw6dz@virginia.edu>
2015-02-23 15:15:33 -0500

    testing course_reader's method

--- course_reader.py
+++ course_reader.py
@@ -8,14 +8,22 @@ def load_course_database(db_name, csv_filename):
     con = lite.connect(db_name)
     with con:
          cur = con.cursor()
-         cur.execute("CREATE TABLE coursedata(deptID TEXT, courseNum INT, semester INT, meetingType TEXT, seatsTaken INT, seatsOffered INT, instructor TEXT)")
+         cur.execute("CREATE TABLE IF NOT EXISTS coursedata(deptID TEXT, courseNum INT, semester INT, meetingType TEXT, seatsTaken INT, seatsOffered INT, instructor TEXT)")
     #for row in csv_f: #every row is a list
     #    print(row)
     for row in csv_f:
         IDtup = (row[0], row[1])
         with con:
             cur = con.cursor()
-            sql_cmd = "INSERT INTO coursedata values(?. ?. row[2], row[3], row[4]. row[5]. row[6])"
+            sql_cmd = "INSERT INTO coursedata values(?, ?, row[2], row[3], row[4], row[5], row[6])"
             cur.execute(sql_cmd, IDtup)
 
-load_course_database("hi", "seas-courses-5years.csv")
+def read_table(db_name, db_table):
+    con = lite.connect(db_name)
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM db_table")
+        rows = cur.fetchall()
+    print(rows)
+
+load_course_database("course1.db", "seas-courses-5years.csv")
